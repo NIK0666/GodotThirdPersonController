8484399afcc0eeeda15d2ab80d0d3dc1778d1610
Nikolay <nik89kol87@gmail.com>
2019-10-26 12:43:37 +0600

    Separate script (create SpringArm script)

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,38 +1,34 @@
 extends KinematicBody
 
-export var Sensitivity_X = 0.1
-export var Sensitivity_Y = 0.1
-export var Zoom_Step = 0.5
-export var Acceleration_Start = 10
-export var Acceleration_Stop = 25
-export var Max_Speed = 250
-export var Rotate_Model_Step = PI * 4
+export var Sensitivity_X: float = 0.1
+export var Sensitivity_Y: float = 0.1
+export var Zoom_Step: float = 0.5
+export var Acceleration_Start: float = 10
+export var Acceleration_Stop: float = 25
+export var Max_Speed: float = 200
+export var Rotate_Model_Step: float = PI * 2.0
 
-const MIN_ROT_Y = -89
-const MAX_ROT_Y = 45
-const ZOOM_MIN = 2
-const ZOOM_MAX = 10
 const GRAVITY = 9.8 * 4
 
-onready var rotate_node = $SpringArm
-onready var model_node = $CSG_Model
+onready var rotate_node: Spatial = $SpringArm
+onready var model_node: Spatial = $CSG_Model
 
-var mouse_relative = Vector2()
-var move_offset = Vector2()
-var move_forward = 0
-var move_right = 0
-var velocity = Vector3()
+var mouse_relative: Vector2 = Vector2()
+var move_offset: Vector2 = Vector2()
+var move_forward: float = 0
+var move_right: float = 0
+var velocity: Vector3 = Vector3()
 
 func _ready():
+	_setup_spring_arm()
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
+func _setup_spring_arm():
+	rotate_node.zoom_step = Zoom_Step
+	rotate_node.sens_x = Sensitivity_X
+	rotate_node.sens_y = Sensitivity_Y
+
 func _input(event):
-	if event is InputEventMouseMotion:
-		mouse_relative += event.relative
-	elif event.is_action_pressed("camera_zoom_in"):
-		zoom_camera(1)
-	elif event.is_action_pressed("camera_zoom_out"):
-		zoom_camera(-1)
 	
 	if event.is_action_pressed("move_forward"):
 		move_offset.x -= 1
@@ -53,36 +49,8 @@ func _input(event):
 		move_offset.y += 1
 
 func _physics_process(delta):
-	if mouse_relative != Vector2.ZERO:
-		rotate_camera(Vector2(mouse_relative.y * delta * Sensitivity_X, mouse_relative.x * delta * Sensitivity_Y))
-		mouse_relative = Vector2()
-
 	if move_offset != Vector2.ZERO or move_forward != 0 or move_right != 0 or !is_on_floor():
 		move(delta)
-
-func rotate_camera(offset):
-	rotate_node.rotation.y -= offset.y
-	
-	if rotate_node.rotation.y > PI:
-		rotate_node.rotation.y -= PI*2
-	elif rotate_node.rotation.y < -PI:
-		rotate_node.rotation.y += PI*2
-	
-	if offset.x <= 0 and rotate_node.rotation_degrees.x >= MAX_ROT_Y:
-		return
-	if offset.x >= 0 and rotate_node.rotation_degrees.x <= MIN_ROT_Y:
-		return
-	
-	rotate_node.rotation.x -= offset.x
-
-func zoom_camera(direction):
-	
-	if direction > 0 and rotate_node.spring_length <= ZOOM_MIN:
-		rotate_node.spring_length = ZOOM_MIN
-	elif direction < 0 and rotate_node.spring_length >= ZOOM_MAX:
-		rotate_node.spring_length = ZOOM_MAX
-	else:
-		rotate_node.spring_length -= direction * Zoom_Step
 
 func move(delta):
 	
@@ -109,9 +77,14 @@ func move(delta):
 		elif move_right < 0:
 			move_right = min(move_right + Acceleration_Stop, 0)
 		
-	var direction = Vector3()
+	var direction: Vector3 = Vector3()
 	direction.x = move_right * delta
 	direction.z = move_forward * delta
+	
+	if move_forward != 0 and move_right != 0:
+		direction.x *= 0.71
+		direction.z *= 0.71
+	
 	direction = direction.rotated(Vector3(0, 1, 0), rotate_node.rotation.y)
 	
 	velocity.x = direction.x
@@ -160,8 +133,6 @@ func rotate_model(delta):
 		model_node.rotation.y = min(model_node.rotation.y + delta * Rotate_Model_Step, target_rotation_y)
 	
 	return true
-
-
 
 
 
